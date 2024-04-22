64017648afda7b6dd5041203b5480e11bff2322c
tabita-emapohus <161648276+tabita-emapohus@users.noreply.github.com>
2024-04-22 22:24:20 +0300

    add Product.java

---
+++ Product.java
@@ -0,0 +1,21 @@
+
+public class Product {
+
+	public String name;
+	public double price;
+	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	
+}
